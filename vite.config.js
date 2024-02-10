de4e9c0990052db1c9eef70fdd07cdfea71ee121
Carlos Berry <carlosberry@trentu.ca>
2024-02-10 00:08:37 -0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/BerryCool.github.io/cois3850",
+  base: "https://berrycool.github.io/cois3850/",
 });
